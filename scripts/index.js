6ca0d55df9c56659d4567c285c5bd4686683706b
NikitaBondarev1 <Bondarev363@yandex.ru>
2023-01-06 16:39:43 +0300

    6

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ const popupContainer = document.querySelector(".popup__container");
 const popupClose = document.querySelector(".popup__close");
 let popupInfo = document.querySelector(".popup__info");
 let nameInput = document.querySelector(".popup__name_input");
-let subtitleWork = document.querySelector(".popup__subtitle_work");
+let subtitleWork = document.querySelector(".popup__subtitle");
 
 buttonEdd.addEventListener("click", (evt) => {
   evt.preventDefault();
@@ -25,7 +25,7 @@ function formSubmit(evt) {
   evt.preventDefault();
 
   profileName.textContent = nameInput.value;
-  subtitleWork.textContent = subtitleWork.value;
+  profileSubtitle.textContent = subtitleWork.value;
   popup.classList.remove("popup_active");
 }
 popupInfo.addEventListener("submit", formSubmit);
